6954e922f7cf9eef76a25bf2c38ff671b704335a
tomlord <aa123593465@gmail.com>
2024-11-05 14:32:32 +0800

    chore: metadata setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,10 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>Kaiti Challenge</title>
+	<meta name="description" content="Writing todo app in sveltekit" />
+	<meta name="keywords" content="sveltekit, todo app" />
+</svelte:head>
+
 {@render children()}
